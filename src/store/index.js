608fbdd24ac13c956890382b9aa04862c2373bf7
namdq2 <namquoc.dev@gmail.com>
2021-06-02 23:22:42 +0700

    remove counter in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from './counter/counterSlice';
 import vocabularyReducer from './vocabulary/vocabularySlice';
 
 export default configureStore({
     reducer: {
-        counter: counterReducer,
         vocabulary: vocabularyReducer,
     },
 });
