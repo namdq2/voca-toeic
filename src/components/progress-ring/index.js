86f2c070fcbfe41e9595e401fc523ee9e284e698
namdq2 <namquoc.dev@gmail.com>
2021-06-08 10:03:41 +0700

    refactoring: convert component to hook

--- src/components/progress-ring/index.js
+++ src/components/progress-ring/index.js
@@ -1,46 +1,36 @@
-import React from 'react';
+function ProgressRing(props) {
+    const {radius, stroke, progress} = props;
 
-class ProgressRing extends React.Component {
-    constructor(props) {
-        super(props);
+    const normalizedRadius = radius - stroke * 2;
+    const circumference = normalizedRadius * 2 * Math.PI;
 
-        const {radius, stroke} = this.props;
+    const strokeDashoffset = circumference - progress / 100 * circumference;
+    const strokeDashoffsets = circumference - 0 / 100 * circumference;
 
-        this.normalizedRadius = radius - stroke * 2;
-        this.circumference = this.normalizedRadius * 2 * Math.PI;
-    }
-
-    render() {
-        const {radius, stroke, progress} = this.props;
-
-        const strokeDashoffset = this.circumference - progress / 100 * this.circumference;
-        const strokeDashoffsets = this.circumference - 0 / 100 * this.circumference;
-
-        return (
-            <svg className="transform -rotate-90"
-                 height={radius * 2}
-                 width={radius * 2}>
-                <circle
-                    stroke="rgba(191, 219, 254, var(--tw-bg-opacity))"
-                    fill="transparent"
-                    strokeWidth={stroke}
-                    strokeDasharray={this.circumference + ' ' + this.circumference}
-                    style={{strokeDashoffsets}}
-                    r={this.normalizedRadius}
-                    cx={radius}
-                    cy={radius}/>
-                <circle
-                    stroke="rgba(59, 130, 246, var(--tw-bg-opacity))"
-                    fill="transparent"
-                    strokeWidth={stroke}
-                    strokeDasharray={this.circumference + ' ' + this.circumference}
-                    style={{strokeDashoffset}}
-                    r={this.normalizedRadius}
-                    cx={radius}
-                    cy={radius}/>
-            </svg>
-        );
-    }
+    return (
+        <svg className="transform -rotate-90"
+             height={radius * 2}
+             width={radius * 2}>
+            <circle
+                stroke="rgba(191, 219, 254, var(--tw-bg-opacity))"
+                fill="transparent"
+                strokeWidth={stroke}
+                strokeDasharray={circumference + ' ' + circumference}
+                style={{strokeDashoffsets}}
+                r={normalizedRadius}
+                cx={radius}
+                cy={radius}/>
+            <circle
+                stroke="rgba(59, 130, 246, var(--tw-bg-opacity))"
+                fill="transparent"
+                strokeWidth={stroke}
+                strokeDasharray={circumference + ' ' + circumference}
+                style={{strokeDashoffset}}
+                r={normalizedRadius}
+                cx={radius}
+                cy={radius}/>
+        </svg>
+    );
 }
 
 export default ProgressRing;
